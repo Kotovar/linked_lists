cf7d9419f37986cacba92455fb16006c2a2e7560
Kotovar <neasit3@gmail.com>
2023-11-02 20:26:11 +0700

    add pop and contains(value) methods

--- script.js
+++ script.js
@@ -59,6 +59,32 @@ class LinkedList {
     }
     return currentNode ?? `The index ${startIndex} out of list`;
   }
+
+  pop() {
+    if (!this.head) {
+      return `No items to delete`;
+    }
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+      return;
+    }
+    let penult = this.at(this.size() - 2);
+    penult.nextNode = null;
+    this.tail = penult;
+  }
+
+  contains(value) {
+    if (!this.head) return `The current LinkedList is empty`;
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        return true;
+      }
+      currentNode = currentNode.nextNode;
+    }
+    return false;
+  }
 }
 
 class Node {
@@ -76,7 +102,9 @@ list.append("Хвост");
 
 list.prepend("Добавил в начало").prepend("Ещё один - самый первый");
 console.log(list);
-// console.log(list.size());
 // console.log(list.getHead());
 // console.log(list.getTail());
-console.log(list.at(2));
+// console.log(list.at(3));
+// list.pop();
+// console.log(list.size());
+console.log(list.contains("Хвост"));
