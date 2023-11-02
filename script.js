6533bd7450deebd8c6781b7b04365954e8de6bee
Kotovar <neasit3@gmail.com>
2023-11-02 22:30:25 +0700

    add insertAt(value, index), removeAt(index) methods

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ class LinkedList {
     this.tail = tail;
   }
 
+  getHead() {
+    return this.head;
+  }
+
   getTail() {
     if (!this.head) return this.head;
     let tail = this.head;
@@ -45,10 +49,6 @@ class LinkedList {
     return itogSize;
   }
 
-  getHead() {
-    return this.head;
-  }
-
   at(index) {
     let startIndex = index;
     let currentNode = this.head;
@@ -85,6 +85,75 @@ class LinkedList {
     }
     return false;
   }
+
+  find(value) {
+    let index = 0;
+    if (!this.head) return `The current LinkedList is empty`;
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        return index;
+      }
+      index++;
+      currentNode = currentNode.nextNode;
+    }
+    return 0;
+  }
+
+  toString() {
+    let currentNode = this.head;
+    let string = "";
+    while (currentNode !== null) {
+      string += `( ${currentNode.value} ) -> `;
+      currentNode = currentNode.nextNode;
+    }
+    return (string += null);
+  }
+
+  insertAt(value, index) {
+    if ((index ^ 0) !== index || index < 0) {
+      return `wrong value`;
+    }
+    const range = this.size();
+    if (index === 0) {
+      this.prepend(value);
+      return;
+    }
+    if (index === range) {
+      this.append(value);
+      return;
+    }
+    if (index > range) {
+      return `Out of range`;
+    }
+    let currentIndex = this.at(index);
+    let newElement = new Node(value, currentIndex);
+    let previousIndex = this.at(index - 1);
+    previousIndex.nextNode = newElement;
+  }
+
+  removeAt(index) {
+    if ((index ^ 0) !== index || index < 0) {
+      return `wrong value`;
+    }
+    const range = this.size() - 1;
+    if (index === 0) {
+      this.head = this.at(index + 1);
+      return;
+    }
+    if (index === range) {
+      this.pop();
+      return;
+    }
+    if (index > range) {
+      return `Out of range`;
+    }
+    let element = this.at(index);
+    let previousIndex = this.at(index - 1);
+    let nextIndex = this.at(index + 1);
+    previousIndex.nextNode = nextIndex;
+    element.nextNode = null;
+  }
 }
 
 class Node {
@@ -101,10 +170,18 @@ list.append("Перед хвостом");
 list.append("Хвост");
 
 list.prepend("Добавил в начало").prepend("Ещё один - самый первый");
-console.log(list);
+list.append("Самый последний");
+// console.log(list);
 // console.log(list.getHead());
 // console.log(list.getTail());
 // console.log(list.at(3));
 // list.pop();
 // console.log(list.size());
-console.log(list.contains("Хвост"));
+// console.log(list.contains("Перед хвостом"));
+// console.log(list.find("Перед хвостом"));
+// console.log(list.toString());
+list.insertAt("НОВЕНЬКИЙ", 8);
+console.log(list.toString());
+list.removeAt(2);
+console.log(list.toString());
+console.log(list);
