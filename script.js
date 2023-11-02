5151ccde5e1a48c1fa1b7deb3e85a17ba07468b9
Kotovar <neasit3@gmail.com>
2023-11-02 18:44:05 +0700

    add prepend, size, head, at(index) methods

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ class LinkedList {
   }
 
   getTail() {
-    if (!this.head) return null;
+    if (!this.head) return this.head;
     let tail = this.head;
     while (tail.nextNode !== null) {
       tail = tail.nextNode;
@@ -19,9 +19,45 @@ class LinkedList {
       this.tail = this.head;
       return this;
     }
-    let previous = this.getTail();
+    let previousTail = this.getTail();
     this.tail = new Node(value, null);
-    previous.nextNode = this.tail;
+    previousTail.nextNode = this.tail;
+  }
+
+  prepend(value) {
+    if (!this.head) {
+      this.head = new Node(value, null);
+      this.tail = this.head;
+      return this;
+    }
+    let currentHead = this.head;
+    this.head = new Node(value, currentHead);
+    return this;
+  }
+
+  size() {
+    let itogSize = 0;
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      itogSize++;
+      currentNode = currentNode.nextNode;
+    }
+    return itogSize;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  at(index) {
+    let startIndex = index;
+    let currentNode = this.head;
+    if (!this.head) return `The current LinkedList is empty`;
+    while (index > 0 && currentNode) {
+      currentNode = currentNode.nextNode;
+      index--;
+    }
+    return currentNode ?? `The index ${startIndex} out of list`;
   }
 }
 
@@ -33,7 +69,14 @@ class Node {
 }
 
 let list = new LinkedList();
-list.append("День");
-// list.append("Ночь");
-// list.append("Вечер");
+list.append("Голова").append("После головы");
+list.append("Средний");
+list.append("Перед хвостом");
+list.append("Хвост");
+
+list.prepend("Добавил в начало").prepend("Ещё один - самый первый");
 console.log(list);
+// console.log(list.size());
+// console.log(list.getHead());
+// console.log(list.getTail());
+console.log(list.at(2));
